ef154cbb0d2ce47ab86b8d542a4bcb231a59787a
linhai.song <songlinhai0543@gmail.com>
2018-03-31 22:31:19 -0700

    check in before fire alarm

---
+++ figure/Mozilla477564.js
@@ -0,0 +1,7 @@
+char * sss_xph_generate(node_t* aNode) {
+   int count=0;
+   for (n = aNode; n ; n = aNode->prev) {
+     if (n->localName == aNode->localName && n->URI == aNode->URI) 
+         count++;
+   } ...
+}//called for every node in a linked list
--- figure/apache34464.c
+++ figure/apache34464.c
@@ -1,7 +1,9 @@
-//indexOf: check if string source[] contains target[]
+//indexOf: check if string source[] contains string target[]
 //sourceLen: the length of string source[]
 //targetLen: the length of string target[]
-while (indexOf(source, sourceLen, target, targetLen) < 0) {
-	//append another character; try again
-	source[sourceLen ++] = getchar();
-}
++ int i = -1 * targetLen
+- while (indexOf(source, 0, sourceLen, target, targetLen) < 0) {
++ while(i++ < 0 || indexOf(source, i, sourceLen, target, targetLen) {
+	  //append another character; try again
+	  source[sourceLen++] = getchar();
+  }
